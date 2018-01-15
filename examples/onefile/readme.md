699a3f562c445d13bc9e383bcee06adbeb106543
deviator <code.viator@gmail.com>
2018-01-15 15:02:11 +0300

    add rmknown

--- examples/onefile/readme.md
+++ examples/onefile/readme.md
@@ -3,3 +3,5 @@
 Возможно, полезные программы
 
 * [ghi](./ghi.d) -- очень простой аналог оригинального [ghi](https://github.com/stephencelis/ghi) (`dub run dusful:ghi`)
+
+* [rmknown](./rmknonw.d) -- удаляет из `.ssh/known_hosts` строку, если в ней присутствует переданная в программу строка
---
+++ examples/onefile/rmknown.d
@@ -0,0 +1,40 @@
+/+ dub.sdl:
+    name "rmknown"
+    type "executable"
+ +/
+/++ Удаляет строку из файла .ssh/known_hosts, если находит в ней
+    переданную программе строку (по замыслу это IP адрес, но может
+    быть любая строка)
+ +/
+import std.stdio;
+import std.path;
+import std.file;
+import std.process : environment;
+import std.algorithm;
+
+enum knownHosts = ".ssh/known_hosts";
+
+int main(string[] args)
+{
+    if (args.length != 2)
+    {
+        stderr.writeln("use:\nrmknown <IP>");
+        return 1;
+    }
+
+    auto IP = args[1];
+
+    auto fname = buildNormalizedPath(environment["HOME"], knownHosts);
+
+    auto orig = fname ~ ".back";
+    rename(fname, orig);
+
+    auto src = File(orig, "r");
+    auto dst = File(fname, "w");
+
+    foreach (ln; src.byLine)
+        if (!ln.canFind(IP))
+            dst.write(ln);
+
+    return 0;
+}
